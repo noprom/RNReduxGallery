9956a61a7c752965bdc4c3025b6e2d1ce900fadf
noprom <tyee.noprom@qq.com>
2016-11-25 09:19:42 +0800

    implement user reducer

---
+++ app/reducers/user.js
@@ -0,0 +1,46 @@
+'use strict';
+
+import * as TYPES from '../actions/types';
+
+const initialState = {
+  isLoggedIn: false,
+  user: {},
+  status: null
+};
+
+export default function user(state = initialState, action) {
+  switch (action.type) {
+    case TYPES.LOGGED_DOING:
+      return {
+        ...state,
+        status: 'doing'
+      }
+      break;
+    case TYPES.LOGGED_IN:
+      return {
+        ...state,
+        isLoggedIn: true,
+        user: action.user,
+        status: 'done'
+      }
+      break;
+    case TYPES.LOGGED_OUT:
+      return {
+        ...state,
+        isLoggedIn: false,
+        user: {},
+        status: null
+      }
+      break;
+    case TYPES.LOGGED_ERROR:
+      return {
+        ...state,
+        isLoggedIn: false,
+        user: {},
+        status: null
+      }
+      break;
+    default:
+      return state;
+  }
+}
