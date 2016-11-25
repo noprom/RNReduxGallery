e4efcc4333cce9933dcff12175a01f7099f59810
noprom <tyee.noprom@qq.com>
2016-11-25 11:18:08 +0800

    add LoginPage

--- app/pages/login.js
+++ app/pages/login.js
@@ -15,4 +15,139 @@ import Spinner from 'react-native-spinkit';
 import {login, skipLogin} from '../actions/user';
 import commonStyle from '../styles/common';
 import loginStyle from '../styles/login';
-// TODO
+
+class LoginPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: 'noprom',
+      password: '1q2w3e4r5t',
+      btnFlag = true
+    }
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextProps.isLoggedIn != this.state.isLoggedIn && nextProps.isLoggedIn == true) {
+      this.refs.modal.close();
+      this.toMain();
+      return false;
+    }
+    if(nextProps.status == 'doing'){
+      //loggining
+      this.refs.modal.open();
+      return false;
+    }
+    if(nextProps.status == 'error' || nextProps.status == 'done'){
+      this.refs.modal.close();
+      return false;
+    }
+    return true;
+  }
+
+  toMain() {
+    const {router} = this.props;
+    router.toMain();
+  }
+
+  handleLogin() {
+    if(!this.state.username || !this.state.password){
+      AlertIOS.alert(
+        'username, password?'
+      );
+      return;
+    }
+    let opt = {
+      'name': this.state.username,
+      'password': this.state.password,
+    };
+    this.props.dispatch(login(opt));
+  }
+
+  handleRegister() {
+    const {dispatch} = this.props;
+    dispatch(skipLogin());
+  }
+
+  onChangeName(text) {
+    this.setState({'username': text});
+  }
+
+  onChangePswd(text) {
+    this.setState({'password': text});
+  }
+
+  render() {
+        return (
+          <View style={[commonStyle.wrapper, loginStyle.loginWrap]}>
+            <Image source={require('../imgs/icons/bg.png')} style={{resizeMode: 'stretch'}}>
+                <View style={loginStyle.loginMain}>
+                    <View style={loginStyle.loginMainCon}>
+                        <View style={loginStyle.comCulture}>
+                            <Text style={[commonStyle.textCenter,{color:'#ccc'}]}>Welcome</Text>
+                            <Text style={[commonStyle.textCenter,{color:'#ccc'}]}>You are the best.</Text>
+                        </View>
+                        <View style={loginStyle.formStyle}>
+                            <View style={[loginStyle.formInput,loginStyle.formInputSplit]}>
+                                <Image source={require('../imgs/icons/user.png')} style={{width:25,height:25,resizeMode: 'contain'}}/>
+                                <TextInput
+                                    ref="login_name"
+                                    placeholder='username'
+                                    style={loginStyle.loginInput}
+                                    onChangeText={this.onChangeName.bind(this)} />
+                            </View>
+                            <View style={loginStyle.formInput}>
+                                <Image source={require('../imgs/icons/passicon.png')} style={{width:25,height:25,resizeMode: 'contain'}}/>
+                                <TextInput
+                                    ref="login_psw"
+                                    style={loginStyle.loginInput}
+                                    secureTextEntry={true}
+                                    placeholder='password'
+                                    onChangeText={this.onChangePswd.bind(this)} />
+                            </View>
+                            <View style={{alignItems: 'flex-end'}}>
+                                <View style={loginStyle.forget}>
+                                <View>
+                                    <Image source={require('../imgs/icons/prompt.png')} style={{width:15,height:15,resizeMode: 'contain',marginRight:10}}/>
+                                </View>
+                                <View >
+                                    <Text style={{color:'#62a2e0', backgroundColor: 'white'}}>forget password?</Text>
+                                </View>
+                                </View>
+                            </View>
+                        </View>
+                        <View style={loginStyle.btn}>
+                            <View style={loginStyle.btnWrap}>
+                                <Text style={loginStyle.loginBtn1} onPress={this.handleLogin.bind(this)}>Log in</Text>
+                            </View>
+                            <View style={loginStyle.btnWrap}>
+                                <Text style={loginStyle.loginBtn2} onPress={this.handleRegister.bind(this)}>Skip</Text>
+                            </View>
+                        </View>
+                    </View>
+                </View>
+            </Image>
+
+           <ModalBox style={[commonStyle.modal,commonStyle.justAlign]}
+                    ref={"modal"} backdropPressToClose={false}
+                     animationDuration={10}
+                     backdrop={true}
+                     backdropOpacity={0}
+                     >
+                <Spinner style={commonStyle.spinner}
+                    isVisible={true}
+                    size={50} type="Arc" color="#FFFFFF"/>
+            </ModalBox>
+          </View>
+        );
+    }
+}
+
+function select(store) {
+  return {
+    isLoggedIn: store.userStore.isLoggedIn,
+    user: store.userStore.user,
+    status: store,userStore.status
+  }
+}
+
+export default connect(select)(LoginPage);
