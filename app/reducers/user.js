df728d46b7fed894f0f4c3edef25f52bb7156829
noprom <tyee.noprom@qq.com>
2016-11-25 09:58:19 +0800

    combine reducer

--- app/reducers/user.js
+++ app/reducers/user.js
@@ -8,7 +8,7 @@ const initialState = {
   status: null
 };
 
-export default function user(state = initialState, action) {
+export default function userReducer(state = initialState, action) {
   switch (action.type) {
     case TYPES.LOGGED_DOING:
       return {
